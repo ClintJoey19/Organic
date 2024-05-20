b5f96bdab14fa31750b442d39ead2cd39b16c025
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-05-20 22:04:03 +0800

    chore: categories

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Categories = () => {
-  return <section>Categories</section>;
+  return (
+    <section className="container mt-4">
+      <h2 className="page-title text-center">Categories</h2>
+      <div className="flex max-sm:flex-col gap-2">
+        <div className="flex-1">Vegetables</div>
+        <div className="flex-1">Fruits</div>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="h-[80vh] max-w-[1220px] m-auto grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="h-full flex flex-col justify-center gap-y-4 max-md:p-4">
+    <section className="h-[80vh] container grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="h-full flex flex-col justify-center gap-y-4 md:p-4">
         <p className="text-primary">Organic 100% Genuine Product Served</p>
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-6xl max-md:text-4xl font-bold">
           Organic & <span className="text-primary">Healthy Food</span> Everyday
         </h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
           reiciendis est temporibus at in harum cupiditate modi, earum omnis ut?
         </p>
-        <div className="mt-4 flex max-md:justify-center gap-2">
+        <div className="mt-4 flex gap-2">
           <Button asChild>
             <Link href="/products">Shop Now</Link>
           </Button>
@@ -23,8 +23,8 @@ const Hero = () => {
           </Button>
         </div>
       </div>
-      <div className="relative max-md:aspect-video overflow-hidden rounded-b-xl">
-        <Image src="/hero.jpg" alt="hero" fill />
+      <div className="relative max-md:max-h-[250px] min-h-[150px] overflow-hidden rounded-xl md:rounded-b-xl">
+        <Image src="/hero.jpg" alt="hero" fill objectFit="cover" />
       </div>
     </section>
   );
